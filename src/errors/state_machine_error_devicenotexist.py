d6d910063e4c1e867ddb426762cff70c8fb9f974
rbiancosilva <rbiancosilva45@gmail.com>
2024-09-10 11:56:25 -0300

    Adding retry recursive method

--- src/errors/state_machine_error_devicenotexist.py
+++ src/errors/state_machine_error_devicenotexist.py
@@ -1,14 +1,23 @@
 import os
 
-from .state_machine_error import StateMachineError
+from .gammu_error import GammuError
 
-
-class StateMachineErrorDeviceNotExist(StateMachineError):
-    def __init__(self, gammu_exception):
+class StateMachineErrorDeviceNotExist(GammuError):
+    def __init__(self, gammu_exception, tries: int):
         super().__init__(gammu_exception)
+        self._tries = tries
+        if self._tries < 3:
+            self.retry_state_machine_handler()
+        else:
+            self.raise_exception()
 
     def raise_exception(self):
-        self._logging_handler.logger.error(f"{self._e}. Rebooting E3531.")
-        os.system("sh /usr/local/bin/usb_modeswitch_reset.sh")
+        self._logging_handler.logger.error(f"{self._e}.")
         self._logging_handler.iterate_status_counter('failure')
         raise Exception(self._e)
+
+    def retry_state_machine_handler(self):
+        self._logging_handler.logger.warning(f"{self._e}. Rebooting E3531.")
+        os.system("sh /usr/local/bin/usb_modeswitch_reset.sh")
+        self._logging_handler.logger.info(f"Rebooted E3531.")
+        return 1
--- src/services/state_machine_handler.py
+++ src/services/state_machine_handler.py
@@ -1,15 +1,16 @@
 import gammu
 
 from src.core import StateMachineStarter
-from src.errors import *
+from src.errors import StateMachineErrorDeviceNotExist, StateMachineError
 
 
 class StateMachineHandler(StateMachineStarter):
-    def __init__(self):
+    def __init__(self, tries: int=0):
         try:
             super().__init__()
         except gammu.ERR_DEVICENOTEXIST as e:
-            StateMachineErrorDeviceNotExist((e.args[0])['Text'])
+            StateMachineErrorDeviceNotExist((e.args[0])['Text'], tries)
+            self.__init__(tries+1)
         except gammu.ERR_DEVICEOPENERROR as e:
             StateMachineError((e.args[0])['Text'])
         except gammu.GSMError as e:
